ddbb1de5c7acf0bcaaab92014d8c5bde62e9c2e8
Dimo Karachorbadzhiev <d_i_m_o@yahoo.com>
2024-01-09 23:09:40 +0200

    set react-hook form

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,23 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Home from './components/Home/home';
+import { Document, Page, View } from '@react-pdf/renderer'
+// import Home from './components/Home/home';
+import myPDF from './components/pdf/DocPDF';
 
 function App() {
  
   return (
     <>
-  <Home />
-
-    </>
+  {/* <Document> */}
+    {/* <Page wrap> */}
+      {/* <View wrap={false}> */}
+       {/* <Home /> */}
+      <myPDF />
+      {/* </View> */}
+    {/* </Page> */}
+  {/* </Document> */}
+  </>
   )
 }
 
